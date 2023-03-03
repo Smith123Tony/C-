7febe11d0beafc710cd25842c68863d69f9eeb3c
Smith123Tony <JK.Tony@outlook.com>
2023-03-03 20:46:35 +0800

    第一个C++程序

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+#define Day 7
+
+int main()
+{
+	/*cout << "hello world" << endl;
+	return 0;*/
+	/*int a = 10;
+	cout << "a=" << a << endl;*/
+
+	/*cout << "Day=" << Day << endl;*/
+
+	cout << "sizeof(int)=" << sizeof(int) << endl;
+	cout << "sizeof(double)=" << sizeof(double) << endl;
+	cout << "sizeof(short)=" << sizeof(short) << endl;
+	cout << "sizeof(float)=" << sizeof(float) << endl;
+	cout << "sizeof(long)=" << sizeof(long) << endl;
+	
+	char ch = 'a';
+	cout << (int)ch << endl;
+
+	return 0;
+}
