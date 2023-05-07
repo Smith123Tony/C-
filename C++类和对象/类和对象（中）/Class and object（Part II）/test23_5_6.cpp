f7420b7f4448aa4a526e53d69e7ca6a25345e349
Smith123Tony <JK.Tony@outlook.com>
2023-05-07 11:46:12 +0800

    C++类和对象（中）。

--- C++类和对象/类和对象（中）/Class and object（Part II）/test23_5_6.cpp
+++ C++类和对象/类和对象（中）/Class and object（Part II）/test23_5_6.cpp
@@ -135,40 +135,151 @@ private:
 //}
 
 
-class Date
-{
-public:
-	//C++是支持全缺省或者半缺省，当然C++也支持函数重载，
-	//语法上无参的和全缺省的函数重载是可以同时存在的，依然是构成函数重载
-	//但是无参调用会存在歧义，因为编译器不知道到底调用哪个
-	Date(int year , int month , int day)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
-	}
-	Date()
-	{
-		_year = 1;
-		_month = 1;
-		_day = 1;
-	}
-	void Print()
-	{
-		cout << _year << "-" << _month << "-" << _day << endl;
-	}
-private:
-	//内置类型
-	//C++11支持，这里并不是初始化，这里只是声明（并没有开辟空间）
-	//故这里给的值是默认的缺省值，是给编译器生成的默认构造函数用的
-	int _year = 2;
-	int _month = 2;
-	int _day = 2;
-};
-int main()
-{
-	Date da1;
-	da1.Print();
-	Date da2(23,5,20);
-	da2.Print();
-}
+//class Date
+//{
+//public:
+//	//C++是支持全缺省或者半缺省，当然C++也支持函数重载，
+//	//语法上无参的和全缺省的函数重载是可以同时存在的，依然是构成函数重载
+//	//但是无参调用会存在歧义，因为编译器不知道到底调用哪个
+//	Date(int year = 3 , int month = 3 , int day = 3)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	/*Date()
+//	{
+//		_year = 1;
+//		_month = 1;
+//		_day = 1;
+//	}*/
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	//内置类型
+//	//C++11支持，这里并不是初始化，这里只是声明（并没有开辟空间）
+//	//故这里给的值是默认的缺省值，是给编译器生成的默认构造函数用的
+//	int _year = 2;
+//	int _month = 2;
+//	int _day = 2;
+//};
+//int main()
+//{
+//	//Date da1;
+//	//da1.Print();
+//	Date da2(23,5,20);
+//	da2.Print();
+//	Date da3(20, 5);
+//	da3.Print();
+//	Date da4(19);
+//	da4.Print();
+//	Date da5;
+//	da5.Print();
+//}
+//class Time
+//{
+//public:
+//	Time()
+//	{
+//		cout << "Time()" << endl;
+//		_hour = 0;
+//		_minute = 0;
+//		_second = 0;
+//	}
+//	void Print()
+//	{
+//		cout << _hour << "-" << _minute << "-" << _second << endl;
+//	}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//private:
+//	// 基本类型(内置类型)
+//	int _year;
+//	int _month;
+//	int _day;
+//	// 自定义类型
+//	Time _t;
+//};
+//int main()
+//{
+//	Date d;
+//	Time ti1;
+//	ti1.Print();
+//	return 0;
+//}
+
+////编译器自动生成的默认构造函数---随机值（但是有的编译器会初始化为0）
+//class Date
+//{
+//public:
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date da1;
+//	da1.Print();
+//}
+
+////只存在全缺省的默认构造函数
+//class Date
+//{
+//public:
+//	Date(int year , int month = 3 , int day = 3)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year = 2;
+//	int _month = 2;
+//	int _day = 2;
+//};
+//int main()
+//{
+//	Date da4(18);
+//	da4.Print();
+//}
+
+//只存在无参的默认构造函数
+//class Date
+//{
+//public:
+//	Date()
+//	{
+//		_year ;
+//		_month ;
+//		_day = 20;
+//	}
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year = 2;
+//	int _month = 2;
+//	int _day = 2;
+//};
+//int main()
+//{
+//	Date da1;
+//	da1.Print();
+//}
